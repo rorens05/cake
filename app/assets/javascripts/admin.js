2cdb7868278cd473f42e348ace4b07340f1bad06
Laurence Bautista <rorens05@gmail.com>
2019-06-23 14:26:04 +0800

    added zoom and revise sizes

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   $("#myTable").tablesorter();
+
+  $(".image-zoom").click(function(){
+    $(".image-zoom").toggleClass("hidden")
+  })
+  $(".product-show-image").click(function(){
+    $(".image-zoom").toggleClass("hidden")
+  })
 });
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,7 +1,14 @@
 class Size < ApplicationRecord
   belongs_to :product
 
-  SIZES = ['Small', 'Medium', 'Large']
+  SIZES = [
+      '6x4", Serves 8-10',
+      '8x4", Serves 14-16', 
+      '10x4", Serves 20-22', 
+      '6x4, 8x4", Serves 22-26', 
+      '8x4, 10x4", Serves 34-38', 
+      '6x4, 8x4, 10x4", Serves 42-48'
+    ]
 
   validates :label, :inclusion => { :in => SIZES }
 
